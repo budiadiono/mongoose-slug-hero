cf470915502218ff38227cd52ba710702ae23d1d
budiadiono <budiadiono@yahoo.com>
2015-12-24 17:55:02 +0700

    Fix test hooks

--- test-helper/drop-create-hooks.js
+++ test-helper/drop-create-hooks.js
@@ -1,44 +1,45 @@
-'use strict';
+'use strict'
 
 /**
  * Mocha hooks to always clear & drop db for each test
  * Thanks to: http://www.scotchmedia.com/tutorials/express/authentication/1/06
- **/ 
+ **/
 
 var mongoose = require('mongoose'),
-	db = 'mongodb://127.0.0.1/slugherodbtest';
+  db = 'mongodb://127.0.0.1/slugherodbtest'
 
 module.exports = function () {
+  before('Clear database', function (done) {
+    function clearDB () {
+      for (var i in mongoose.connection.collections) {
+        mongoose.connection.collections[i].remove(function () {})
+      }
+      setTimeout(function () {
+        return done()
+      }, 500)
+    }
 
-	before('Clear database', function (done) {
+    if (mongoose.connection.readyState === 0) {
+      mongoose.connect(db, function (err) {
+        if (err) {
+          throw err
+        }
+        return clearDB()
+      })
+    } else {
+      return clearDB()
+    }
 
-		function clearDB() {			
-			for (var i in mongoose.connection.collections) {
-				mongoose.connection.collections[i].remove(function () { });
-			}
-			return done();
-		}
+  })
 
-		if (mongoose.connection.readyState === 0) {
-			mongoose.connect(db, function (err) {
-				if (err) {
-					throw err;
-				}
-				return clearDB();
-			});
-		} else {
-			return clearDB();
-		}
-		
-	});
+  after('Drop database', function (done) {
+    setTimeout(function () {
+      mongoose.connection.db.dropDatabase(function (err) {
+        if (err) return done(err)
+        mongoose.disconnect(done)
+      })
 
-	after('Drop database', function (done) {
-		
-		mongoose.connection.db.dropDatabase(function (err) {
-			if (err) return done(err);			
-			mongoose.disconnect(done);
-		});
-		
-	});
+    }, 500)
+  })
 
 }
